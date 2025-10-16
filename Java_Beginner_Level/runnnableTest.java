9369c8d36c8a03765d9258411dfcc75698b2252f
bugfreehemantkoli <hemant.koli.official@gmail.com>
2025-10-16 15:57:12 +0530

    Example of Runnable Interface(class NAME implemets Runnable in Java.

---
+++ Java_Beginner_Level/runnnableTest.java
@@ -0,0 +1,16 @@
+class A implements Runnable{
+    public void run(){
+        for(int i=1; i<=10; i++){
+            System.out.println("Thread A "+i);
+        }
+        System.out.println("End of Thread A.");
+    }
+}
+class runnnableTest{
+    public static void main(String args[]){
+        A aa = new A();
+        Thread tt = new Thread(aa);
+        tt.start();
+        System.out.println("End of main Thread.");
+    }
+}
