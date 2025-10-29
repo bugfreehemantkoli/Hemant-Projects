52e1ab2fd7b5f055924e68e3f0b9018a29714a0a
bugfreehemantkoli <hemant.koli.official@gmail.com>
2025-10-29 18:42:28 +0530

    Example of Wrapper Class in Java.

---
+++ Java_Beginner_Level/Wrap.java
@@ -0,0 +1,14 @@
+class Wrap{
+    public static void main(String[] args) {
+        int x = Integer.parseInt("123");
+        System.out.println(x);
+
+        Integer i1 = Integer.valueOf("1000011",2);
+        int z = i1.intValue();
+        System.out.println(z);
+
+        Double x1 = Double.valueOf("56");
+        double y = x1.doubleValue();
+        System.out.println(y);
+    }
+}
