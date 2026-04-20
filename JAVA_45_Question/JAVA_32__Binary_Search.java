969d6bdb6343ac3579dbf2a80af1a17aba223734
bugfreehemantkoli <hemant.koli.official@gmail.com>
2026-04-20 10:32:41 +0530

    Implement binary search program in Java with sorting and user interaction

--- JAVA_45_Question/JAVA_32__Binary_Search.java
+++ JAVA_45_Question/JAVA_32__Binary_Search.java
@@ -1,38 +1,66 @@
 //Java program to implement binary search
-//comment
 
 import java.util.Scanner;
 import java.util.Arrays;
 
 class JAVA_32__Binary_Search{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter the Array Size : ");
-		int n = sc.nextInt();
-		
-		int arr[] = new int[n];
-		
-		for(int i=0; i<n; i++){
-			System.out.print("Enter the Element ("+(i+1)+") : ");
-			arr[i] = sc.nextInt();
-		}
-		
-		System.out.print("\nArray Befor All Elements : ");
-		for(int i=0; i<n; i++){
-			System.out.print(arr[i]+" ");
-		}
-		
-		Arrays.sort(arr);
-		
-		System.out.print("\nArray After All Elements : ");
-		for(int i=0; i<n; i++){
-			System.out.print(arr[i]+" ");
-		}
-		
-		System.out.print("Enter the value to find : ");
-		int value = sc.nextINt();
-		
-		sc.close();
-	}
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Array Size : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the Element (" + (i + 1) + ") : ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("\nArray Before Sorting : ");
+        printArray(arr);
+
+        Arrays.sort(arr);
+
+        System.out.print("\nArray After Sorting  : ");
+        printArray(arr);
+
+        System.out.print("\nEnter the value to find : ");
+        int value = sc.nextInt();
+
+        int result = binarySearch(arr, value);
+
+        if (result == -1) {
+            System.out.println("Not Found Element!");
+        } else {
+            System.out.println("'" + value + "' Value Found at Position " + (result + 1));
+        }
+
+        sc.close();
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+
+            if (arr[mid] == target) {
+                return mid;
+            }else if (arr[mid] < target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
